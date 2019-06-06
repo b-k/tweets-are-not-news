2cf49d0667ffb0898dfe641db2e072ca88c00f5f
b-k <fluffmail@f-m.fm>
2019-06-05 21:43:59 -0400

    Revise stopword list; blank blocks instead of removing them; clean up.

--- notweets.user.js
+++ notweets.user.js
@@ -2,32 +2,29 @@
 // @name        Tweets are not news
 // @namespace   lambast.blast.slam
 // @description Removes items from news sites that are about one person insulting another.
-// @include     /https://.*(washingtonpost.com|wapo.com|nyt.com|nytimes.com)/
+// @include     /https://.*(washingtonpost|wapo|nyt|nytimes).com/
 // @version     1
 // @grant       none
 // ==/UserScript==
 
-var excludes=["lambasts", "blasts", "slams", "hits back at", "Trump"];
+var excludes=["tweet", "lambasts", "blasts", "slams", "hits back at", "berates", "assails"];
 
 function check_excludes(text){
-  for (var x=0; x< excludes.length; x++){
-      if (text.includes(excludes[x])){
+  for (var x=0; x< excludes.length; x++)
+      if (text.includes(excludes[x]))
         return true;
-      }
-  }
   return false;
 }
 
-  console.log("hi hi");
-var headline = document.querySelectorAll('.flex-stack,.assetWrapper');
-for (var i = 0; i < headline.length; i++){
-  var links=headline[i].querySelectorAll('a');
+console.log("Checking for headlines about tweets.");
+
+var blocks = document.querySelectorAll('.flex-stack,.assetWrapper');//flex-stack for WaPo, assetWrapper for NYT
+for (var i = 0; i < blocks.length; i++){
+  var links=blocks[i].querySelectorAll('a');
   for (var j = 0; j < links.length; j++){
     if (check_excludes(links[j].innerText)){
-        console.log("Removing " + links[j].innerText);
-        console.log(headline[i]);
-        headline[i].remove();
-       // headline[i].parentNode.removeChild(headline[i]);
+        console.log("Removing '" + links[j].innerText + "'");
+        blocks[i].style.cssText="display: none";
         break;
     }
   }
